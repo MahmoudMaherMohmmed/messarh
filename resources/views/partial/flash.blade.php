3706de5519bc81eb492476a859200e4a4559b100
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-11-29 15:40:39 +0200

    update chat style

--- resources/views/partial/flash.blade.php
+++ resources/views/partial/flash.blade.php
@@ -1,20 +1,20 @@
-  <br><br>
+@if (Session::has('success'))
+    <br>
+    <div class="alert alert-success {{ Session::has('important_msg') ? 'alert-important' : '' }}">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {{ Session::get('success') }}
+    </div>
+@elseif(Session::has('failed'))
+    <br>
+    <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {{ Session::get('failed') }}
+    </div>
+@endif
 
-
-  @if (Session::has('success'))
-      <div class="alert alert-success {{ Session::has('important_msg') ? 'alert-important' : '' }}">
-          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-          {{ Session::get('success') }}
-      </div>
-  @elseif(Session::has('failed'))
-      <div class="alert alert-danger">
-          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-          {{ Session::get('failed') }}
-      </div>
-  @endif
-
-  @if (session('status'))
-      <div class="alert alert-success" role="alert">
-          {{ session('status') }}
-      </div>
-  @endif
+@if (session('status'))
+    <br>
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+@endif
