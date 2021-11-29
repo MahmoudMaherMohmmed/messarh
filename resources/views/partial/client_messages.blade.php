b414c6450002d0fedfc1ce17ab93894a0601771a
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-11-29 15:21:32 +0200

    update chat

--- resources/views/partial/client_messages.blade.php
+++ resources/views/partial/client_messages.blade.php
@@ -2,7 +2,7 @@
     @foreach($messages as $message)
         @if($message->action == 0)
             <!-- Sender Message-->
-            <div class="media w-50 mb-3"><img src="{{isset($message->client->image) && $message->client->image!=null ? url($message->client->image) : https://bootstrapious.com/i/snippets/sn-chat/avatar.svg}}" alt="user" width="50" class="rounded-circle">
+            <div class="media w-50 mb-3"><img src="{{isset($message->client->image) && $message->client->image!=null ? url($message->client->image) : 'https://bootstrapious.com/i/snippets/sn-chat/avatar.svg'}}" alt="user" width="50" class="rounded-circle">
                 <div class="media-body ml-3">
                     <div class="bg-light rounded py-2 px-3 mb-2">
                     <p class="text-small mb-0 text-muted">{{$message->message}}</p>
