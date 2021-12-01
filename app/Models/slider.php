0172d2d2f6a02302f5e019aa00c816e16b7671d3
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-12-01 12:47:23 +0200

    update slider

--- app/Models/slider.php
+++ app/Models/slider.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Translatable;
 
-class slider extends Model
+class Slider extends Model
 {
     use Translatable, HasFactory;
 
