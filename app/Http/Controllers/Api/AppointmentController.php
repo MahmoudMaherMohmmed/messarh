bcf1cfa6fea129a49dae1118162c58aab1758d54
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-12-08 09:43:37 +0200

    check appointment date

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -97,8 +97,8 @@ class AppointmentController extends Controller
         $appointments = [];
 
         if(isset($request->date) && $request->date!=null){
-            dd($this->formatDate($request->date), Carbon::now()->format('Y-m-d'), $this->formatDate($request->date) == Carbon::now()->format('Y-m-d'));
             if($this->formatDate($request->date) == Carbon::now()->format('Y-m-d')){
+                dd(Carbon::now()->format('H:i A'));
                 $appointments = Appointment::where('doctor_id', $request->doctor_id)->where('date', $this->formatDate($request->date))->where('from', '>', Carbon::now()->format('H:i A'))->where('status', 0)->get(['id', 'from', 'to', 'status']);
             }else{
                 $appointments = Appointment::where('doctor_id', $request->doctor_id)->where('date', $this->formatDate($request->date))->where('status', 0)->get(['id', 'from', 'to', 'status']);
